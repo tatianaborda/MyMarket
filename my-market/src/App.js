ead71f175601d3d9f8febcd10becb0134dc1ff75
Tatiana Borda <56237357+tatianaborda@users.noreply.github.com>
2021-03-11 20:33:26 -0300

    éter tienda de Tatiana Borda

---
+++ my-market/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { NavBar } from './components/NavBar.js'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <NavBar>
+        
+      </NavBar>
+      </div>
+    </div>
+  );
+}
+
+export default App;
